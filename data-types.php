30a5265d49eac79692a7faa2fe88918ecbf5d259
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2024-09-06 05:47:20 +0530

    Started to Learn PHP

---
+++ data-types.php
@@ -0,0 +1,29 @@
+<?php
+    // String
+    $x = "Hello world!";
+    $y = 'Hello world!';
+
+    var_dump($x);
+    echo "<br><br>";
+    var_dump($y);
+
+    echo "<br><br>";
+    // integer
+    $x = 5985;
+    var_dump($x);
+
+    // float
+    echo "<br><br>";
+    $Z = 10.123;
+    var_dump($Z);
+
+    // Boolean
+    echo "<br><br>";
+    $u = true;
+    var_dump($y);
+
+    // Arrays
+    echo "<br><br>";
+    $cars = array("Volvo","BMW","Toyota");
+    var_dump($cars)
+?>
---
+++ variables.php
@@ -0,0 +1,11 @@
+<?php
+    $txt = "Sandaru Hansaka";
+    echo "My name is $txt <br>";
+
+    echo "My name is" .$txt."<br>";
+
+    // sum of variables
+    $x = 5;
+    $y = 4;
+    echo "Sum of vaviables ".$x + $y;
+?>
