6e86621ae9fc6f89898afccacce0cec969f62b92
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2024-09-29 08:49:31 +0530

    A feature used to execute the same SQL statements repeatedly with high efficiency

---
+++ MYSQL/prepared_statements.php
@@ -0,0 +1,48 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "myDB";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // prepare and bind
+    $stmt = $conn->prepare("INSERT INTO MyGuest (firstname, lastname, email) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $firstname, $lastname, $email);
+    
+    // The "sss" argument lists the types of data that the parameters are. The s character tells mysql that the parameter is a string.
+
+    // The argument may be one of four types:
+
+    // i - integer
+    // d - double
+    // s - string
+    // b - BLOB
+
+    // set parameters and execute
+    $firstname = "John";
+    $lastname = "Doe";
+    $email = "john@example.com";
+    $stmt->execute();
+
+    $firstname = "Mary";
+    $lastname = "Moe";
+    $email = "mary@example.com";
+    $stmt->execute();
+
+    $firstname = "Julie";
+    $lastname = "Dooley";
+    $email = "julie@example.com";
+    $stmt->execute();
+
+    echo "New records created successfully";
+
+    $stmt->close();
+    $conn->close();
+?>
